04cd349b284a4ed200697643a0faa71878720e2f
dpeter99 <dpeter99@gmail.com>
2025-08-12 12:24:37 +0200

    Test with updated local build of dell package

--- root/setup/scripts/modules.d/070-dell-fprint.sh
+++ root/setup/scripts/modules.d/070-dell-fprint.sh
@@ -1,3 +1,3 @@
 
 rpm -e --nodeps libfprint
-dnf install -y libfprint-tod libfprint-tod-broadcom
+dnf install -y libfprint-tod /setup/scripts/modules.d/070-dell-fprint/libfprint-tod-broadcom-5.15.285-1.fc40.x86_64.rpm
